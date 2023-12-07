e307343940ebdd05ea795db41763419e5b7c6115
guy-jerome <aaronjamesroberts@gmail.com>
2023-12-07 16:57:04 -0600

    Added all of the tests

--- gilded-rose.js
+++ gilded-rose.js
@@ -47,11 +47,11 @@ export class Pass extends Item {
 
 export let items = [];
 
-items.push(new Item("+5 Dexterity Vest", 10, 20));
-items.push(new Chee("Aged Brie", 2, 0));
-items.push(new Item("Elixir of the Mongoose", 5, 7));
+items.push(new BasicItem("+5 Dexterity Vest", 10, 20));
+items.push(new Cheese("Aged Brie", 2, 0));
+items.push(new BasicItem("Elixir of the Mongoose", 5, 7));
 items.push(new LegendaryItem("Sulfuras, Hand of Ragnaros", 0, 80));
-items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+items.push(new Pass("Backstage passes to a TAFKAL80ETC concert", 15, 20));
 items.push(new ConjuredItem("Conjured Mana Cake", 3, 6));
 
 export const updateQuality = () => {
--- gilded-rose.spec.js
+++ gilded-rose.spec.js
@@ -1,6 +1,8 @@
 import { expect, describe, it } from "vitest";
 import { Item, ConjuredItem, items, updateQuality } from "./gilded-rose.js";
 
+
+//Generic Test
 function testUpdateQuality(ClassName, initialSellIn, initialQuality, expectedSellIn, expectedQuality, name = "Item Name"){
   const testItem = new ClassName(name, initialSellIn, initialQuality)
   items.push(testItem)
@@ -12,18 +14,6 @@ function testUpdateQuality(ClassName, initialSellIn, initialQuality, expectedSel
 }
 
 
-describe("updateQuality", () => {
-  it("reduces quality and sellIn of basic items by 1", () => {
-    const testItem = new Item("basic", 5, 3);
-    items.push(testItem);
-
-    updateQuality();
-
-    expect(testItem.sellIn).toBe(4);
-    expect(testItem.quality).toBe(2);
-  });
-});
-
 //Sub classes
 //Basic item
 //Cheese
@@ -32,32 +22,61 @@ describe("updateQuality", () => {
 //Conjured Item
 
 // Check to ensure quality is not a negative integer
-
 // Item quality cannot increase above 50 Unless Sulfaras Ragnaros
-
 // Conjured items degrade twice as fast
-
 // If sellIn days are less than 0 quality degrades 2x
-
 // Brie increases in quality 
 
+
+// Basic Items Test Cases
+// -- Basic items reduce sellIn and Quality by 1
+// -- Basic items reduce Quality by 2 after sellIn date has been reached
+// -- Basic items Quality cannot go below 0
+
+describe("updateQuality", () => {
+  it("reduces quality and sellIn of basic items by 1", () => {
+    testUpdateQuality(BasicItem,5,3,4,2);
+  });
+});
+
+describe("updateQuality", () => {
+  it("reduces quality by two after the sellIn is below 0", () => {
+    testUpdateQuality(BasicItem,-2,8,-3,6);
+  });
+});
+
+describe("updateQuality", () => {
+  it("quality is not reduced below zeo", () => {
+    testUpdateQuality(BasicItem,5,0,4,0);
+  });
+});
+
+
 // Conjured Items Test Cases
+// -- Conjured items degrade quality by 2 before SellIn date reached
+// -- Conjured items degrade quality by 4 after SellIn date reached
+// -- Conjured items don't degrade beyond 0 
 
 describe('updateQuality', () => {
   it('reduces quality by 2 and sellIn of conjured items by 1', () => {
-    // tests to ensure conjured items degrade by 2x prior to sellIn date
     testUpdateQuality(ConjuredItem, 4,4,3,2);
   })
 })
 
 describe('updateQuality', () => {
   it('reduces quality by 2 and sellIn of conjured items by 1', () => {
-    // tests to ensure conjured items degrade by 4x after sellIn date
     testUpdateQuality(ConjuredItem,-1,4,-2,0);
   })
 })
 
+describe('updateQuality', () => {
+  it('reduces quality by 0 and sellIn of conjured items by 1')
+  testUpdateQuality(ConjuredItem,-4,0,-5,0)
+})
+
 // Cheese Item Test Cases
+// -- Cheese items do not increase quality beyond 50
+// -- Cheese items increase in quality as sellIn decreases
 
 describe('updateQuality', ()=>{
   it("Checks if the quality of the cheese class doesn't increase to be over 50", () => {
@@ -72,22 +91,40 @@ describe('updateQuality', ()=>{
 })
 
 // Legendary Item Test Cases
+// -- Legendary Items have no sellIn date, and are immune to quality change
 
 describe('updateQuality', () => {
   it('Checks to ensure Legendary Items are immune to degredation and quality change' , () => {
-    const testItem = new LegendaryItem('Legendary',0,80 )
     testUpdateQuality(LegendaryItem, 0,80,0,80)  
   })
 })
 
 //Back Stage Pass
-//  - `quality` increases by `2` when there are `10` days or less left before the concert.
+// - `quality` increases instead of decreases as sellin day increases
+// - `quality` increases by `2` when there are `10` days or less left before the concert.
 // - `quality` increases by `3` when there are `5` days or less left before the concert.
 // - `quality` drops to `0` after the concert.
 
 describe('updateQuality', () => {
   it('Checks to ensure that back stage passes increase in value as sell in decreases' , () => {
-    const testItem = new LegendaryItem('Legendary',0,80 )
-    testUpdateQuality(LegendaryItem, 0,80,0,80)  
+    testUpdateQuality(Pass, 15,10,14,11)  
+  })
+})
+
+describe('updateQuality', () => {
+  it('Checks to ensure that back stage passes increase by 2 in value when there are `10` days or less left before the concert sell in decreases' , () => {
+    testUpdateQuality(Pass, 10,10,9,12)  
+  })
+})
+
+describe('updateQuality', () => {
+  it('Checks to ensure that back stage passes increase by 3 in value when there are `5` days or less left before the concert sell in decreases' , () => {
+    testUpdateQuality(Pass, 5,10,4,13)  
+  })
+})
+
+describe('updateQuality', () => {
+  it('Checks to ensure that back stage passes increase by 3 in value when there are `5` days or less left before the concert sell in decreases' , () => {
+    testUpdateQuality(Pass, 0,200,-1,0)  
   })
 })
